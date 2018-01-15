2a2a2ef92050a460699c2dfebe54ea5d06d08e35
Alexander_Morozov <alexander_morozov@epam.com>
2018-01-15 17:44:15 +0300

    Implementation of S2I for nginx

--- openshift/cicd/pipelines/stages/run-build-config/frontend.groovy
+++ openshift/cicd/pipelines/stages/run-build-config/frontend.groovy
@@ -3,8 +3,8 @@ def run(vars) {
         openshift.withCluster() {
             openshift.withProject() {
                 if (!openshift.selector("buildconfig", "${vars.serviceType}").exists())
-                    openshift.newBuild("--name=${vars.serviceType}", "--image-stream=nodejs-6-centos7", "--binary=true")
-                openshift.selector("bc", "${vars.serviceType}").startBuild("--wait=true")
+                    openshift.newBuild("--name=${vars.serviceType}", "--image-stream=s2i-nginx:latest", "--binary=true", "-e NGINX_STATIC_DIR=public")
+                openshift.selector("bc", "${vars.serviceType}").startBuild("--from-dir=${vars.serviceDir}", "--wait=true")
                 openshift.tag("ci-cd/${vars.serviceType}:latest","sit/${vars.serviceType}:latest")
             }
         }
