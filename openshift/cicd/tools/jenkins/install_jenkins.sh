bce9d30f899cbf6954258aa3ada16179493097ab
Viktor_Voronin <Viktor_Voronin@epam.com>
2018-01-16 14:11:59 +0300

    Openshift jenkins improvement

--- openshift/cicd/tools/jenkins/install_jenkins.sh
+++ openshift/cicd/tools/jenkins/install_jenkins.sh
@@ -10,13 +10,13 @@ oc policy add-role-to-user edit admin -n $1
 oc create -f jenkins.yaml -n $1
 
 # create app
-oc new-app jenkins-persistent-glusterfs --param=MEMORY_LIMIT=1Gi -e INSTALL_PLUGINS=analysis-core:1.92,findbugs:4.71,pmd:3.49,checkstyle:3.49,dependency-check-jenkins-plugin:2.1.1,htmlpublisher:1.14,jacoco:2.2.1,analysis-collector:1.52 -n $1
-
-# Create SI project
-oc new-project SI --display-name="SI"
-
-# add permission to admin
-oc policy add-role-to-user edit admin -n SI
+oc new-app jenkins-persistent-glusterfs -e INSTALL_PLUGINS=analysis-core:1.92,findbugs:4.71,pmd:3.49,checkstyle:3.49,dependency-check-jenkins-plugin:2.1.1,htmlpublisher:1.14,jacoco:2.2.1,analysis-collector:1.52 -n $1
 
 
-# Give Jenkins access to SI project
+oc delete deploymentconfig -n ci-cd jenkins
+oc delete route -n ci-cd jenkins
+oc delete pvc -n ci-cd jenkins
+oc delete service -n ci-cd jenkins
+oc delete service -n ci-cd jenkins-jnlp
+oc delete serviceaccount -n ci-cd jenkins
+oc delete rolebinding -n ci-cd jenkins_edit
