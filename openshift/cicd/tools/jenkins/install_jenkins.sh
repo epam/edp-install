e7310f764816adf159c7004d64465da53645e467
Viktor_Voronin <Viktor_Voronin@epam.com>
2018-01-18 11:06:16 +0300

    Gerrit file mount fix Some oc commands in install_jenkins.sh for future automation

--- openshift/cicd/tools/jenkins/install_jenkins.sh
+++ openshift/cicd/tools/jenkins/install_jenkins.sh
@@ -12,7 +12,7 @@ oc create -f jenkins.yaml -n $1
 # create app
 oc new-app jenkins-persistent-glusterfs -e INSTALL_PLUGINS=analysis-core:1.92,findbugs:4.71,pmd:3.49,checkstyle:3.49,dependency-check-jenkins-plugin:2.1.1,htmlpublisher:1.14,jacoco:2.2.1,analysis-collector:1.52 -n $1
 
-
+# Remove all Jenkins objects
 oc delete deploymentconfig -n ci-cd jenkins
 oc delete route -n ci-cd jenkins
 oc delete pvc -n ci-cd jenkins
@@ -21,6 +21,8 @@ oc delete service -n ci-cd jenkins-jnlp
 oc delete serviceaccount -n ci-cd jenkins
 oc delete cm -n ci-cd jenkins-slaves
 oc delete rolebinding -n ci-cd jenkins_edit
+
+# Remove all Nexus objects
 oc delete deploymentconfig -n ci-cd nexus
 oc delete route -n ci-cd nexus
 oc delete pvc -n ci-cd nexus-pv
@@ -28,6 +30,17 @@ oc delete service -n ci-cd nexus
 oc delete serviceaccount -n ci-cd nexus
 oc delete job -n ci-cd nexus-integration
 
+# Remove all Gerrit objects
+oc delete dc -n ci-cd gerrit
+oc delete dc -n ci-cd gerrit-db
+oc delete route -n ci-cd gerrit
+oc delete service -n ci-cd gerrit
+oc delete service -n ci-cd gerrit-db
+oc delete pvc -n ci-cd gerrit-data
+oc delete pvc -n ci-cd gerrit-db
+oc delete cm -n ci-cd gerrit
+oc delete serviceaccount -n ci-cd gerrit
 
+# Add permissions for Sonar and Gerrit service accounts
 oc -n ci-cd adm policy add-scc-to-user anyuid -z sonar
 oc -n ci-cd adm policy add-scc-to-user anyuid -z gerrit
