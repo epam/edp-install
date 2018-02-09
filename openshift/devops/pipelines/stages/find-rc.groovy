22ed2b37b98b29917883a5ab99a3b43d8c6c1507
Viktor_Voronin <Viktor_Voronin@epam.com>
2018-02-09 15:59:51 +0300

    First release
    Change-Id: I1e6ea304dbe84431aad07a669daf769c66a28141

--- openshift/devops/pipelines/stages/find-rc.groovy
+++ openshift/devops/pipelines/stages/find-rc.groovy
@@ -3,13 +3,12 @@
  * @param vars object with all pipeline variables
  */
 def run(vars) {
-    def RC1 = sh(
+    vars['RC'] = sh(
                 script: """
                         git ls-remote --heads origin | grep RC | grep -o '\\..*[0-9]' | awk -F "." '{print \$3}' | sort -n -r | head -n 1
                     """,
                 returnStdout: true
         ).trim()
-    vars['RC']=RC1
     if (vars.RC=="")
         vars['RCnum']=1
     else
