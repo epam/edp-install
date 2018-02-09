405d7aa7835d73d5fff1aed524ade4ad789b9831
Alexander_Morozov <alexander_morozov@epam.com>
2018-02-09 17:56:35 +0300

    Fixed clean stage logic
    Change-Id: Icdcd6465dede99f5eb9dfd7c668fec629a9bf59c

--- openshift/devops/pipelines/stages/delete-environment.groovy
+++ openshift/devops/pipelines/stages/delete-environment.groovy
@@ -5,6 +5,7 @@
 def run(vars) {
     dir("${vars.workDir}") {
         sh "oc delete project ${vars.ocProjectName}"
+        sh "oc -n ${vars.ocProjectName} get pods | grep -v NAME | awk '{print \$1}' | xargs oc -n ${vars.ocProjectName} delete pod --force --grace-period=0"
     }
     this.result = "success"
 }
