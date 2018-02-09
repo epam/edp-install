d2ba1dc28b856a102749db1096ab53411e083ab5
Viktor_Voronin <Viktor_Voronin@epam.com>
2018-02-09 12:36:02 +0300

    First release
    Change-Id: If7c7cd1defb5d1cbe21da9d742934c27a51c41d9

--- openshift/devops/pipelines/Jenkinsfile_Gerrit_Release.groovy
+++ openshift/devops/pipelines/Jenkinsfile_Gerrit_Release.groovy
@@ -9,79 +9,34 @@ tmpDir = RandomStringUtils.random(10, true, true)
 vars = [:]
 vars['npmRegistry'] = "http://nexus:8081/repository/npm-all/"
 commonLib = null
-
-def getServiceType() {
-    getChangeApiUrl = "http://${GERRIT_HOST}:8080/a/changes/?q=${GERRIT_CHANGE_ID}&o=CURRENT_REVISION&o=CURRENT_FILES"
-    def authString
-    withCredentials([usernamePassword(credentialsId: 'gerrit-http', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        authString = "${USERNAME}:${PASSWORD}".getBytes().encodeBase64().toString()
-    }
-    def connection = getChangeApiUrl.toURL().openConnection()
-    connection.setRequestProperty("Authorization", "Basic ${authString}")
-    def parsedJson = new JsonSlurperClassic().parseText(connection.content.text.replaceAll('\\)]}\'', '').trim())
-
-    def paths = parsedJson[0].revisions[GERRIT_PATCHSET_REVISION].files.keySet()
-    echo "[JENKINS][DEBUG] Changed paths - ${paths}"
-
-    def match = paths.join("\n") =~ /.*\/(backend|frontend|database)\/.*/
-    return match[0][1]
-}
+PIPELINES_PATH_DEFAULT = "openshift/devops/pipelines"
 
 node("master") {
-    if (!env.PIPELINES_PATH)
-        error ("[JENKINS][ERROR] PIPELINES_PATH variable is not defined, please check.")
+    vars['pipelinesPath'] = env.PIPELINES_PATH ? PIPELINES_PATH : PIPELINES_PATH_DEFAULT
+    vars['devopsRoot'] = "${workspace}@script"
 
-    vars['pipelinesPath'] = PIPELINES_PATH
-
-    def workspace = "${WORKSPACE.replaceAll("@", "")}@script"
-    dir("${workspace}@script") {
-        stash name: 'data', includes: "${vars.pipelinesPath}/**", useDefaultExcludes: false
-        commonLib = load "${vars.pipelinesPath}/libs/common.groovy"
-    }
-    vars['serviceType'] = getServiceType()
-    println("[JENKINS][DEBUG] Service type to build - ${vars.serviceType}")
-}
-
-buildNode = "backend"
-if (vars.serviceType == "frontend")
-    buildNode = "frontend"
-
-node("${buildNode}") {
-    stage("INITIALIZATION") {
-
-        vars['autoUser'] = env.AUTOUSER ? AUTOUSER : "Auto_EPMC-JAVA_VCS"
-        vars['credentials'] = env.CREDENTIALS ? CREDENTIALS : "gerrit-key"
-
-        vars['devopsRoot'] = new File("/tmp/${tmpDir}")
-        vars['mavenSettings'] = "${vars.pipelinesPath}/settings/maven/settings.xml"
-
-        vars['appPath'] = env.APP_PATH ? APP_PATH : 'openshift/cicd/examples/3-tier-app'
-        vars['serviceBranch'] = GERRIT_BRANCH
-        vars['gerritChange'] = "change-${GERRIT_CHANGE_NUMBER}-${GERRIT_PATCHSET_NUMBER}"
-        vars['serviceDir'] = "${WORKSPACE}/services"
-        vars['gitMicroservicesUrl'] = "ssh://${vars.autoUser}@${GERRIT_HOST}:${GERRIT_PORT}/${GERRIT_PROJECT}"
-
-        //Download DevOps scripts
-        try {
-            dir("${vars.devopsRoot}") {
-                unstash 'data'
-            }
-        } catch (Exception ex) {
-            commonLib.failJob("[JENKINS][ERROR] Devops repository unstash has failed. Reason - ${ex}")
+    dir("${vars.devopsRoot}/${vars.pipelinesPath}/stages/") {
+        stage("CHECKOUT") {
+            stage = load "gerrit-checkout.groovy"
+            stage.run(vars)
         }
 
-        currentBuild.displayName = "${currentBuild.number}-${vars.serviceBranch}(${vars.gerritChange})"
+        stage("FIND LAST RC") {
+            stage = load "deploy-environment.groovy"
+            stage.run(vars)
+        }
 
-        currentBuild.description = """Service: ${vars.serviceType}
-Branch: ${vars.serviceBranch}
-"""
+        stage("CREATE BRANCH") {
+            stage = load "deploy-environment.groovy"
+            stage.run(vars)
+        }
+
+        stage("PUSH TO GERRIT") {
+            stage = load "deploy-environment.groovy"
+            stage.run(vars)
+        }
+
     }
 
-    //dir("${vars.devopsRoot}/${vars.pipelinesPath}/stages/gerrit-checkout/") { commonLib.runStage("CHECKOUT", vars) }
-    //dir("${vars.devopsRoot}/${vars.pipelinesPath}/stages/compile/") { commonLib.runStage("COMPILE", vars) }
-    //dir("${vars.devopsRoot}/${vars.pipelinesPath}/stages/unit-test/") { commonLib.runStage("UNIT-TESTS", vars) }
-    //dir("${vars.devopsRoot}/${vars.pipelinesPath}/stages/sonar-preview/") { commonLib.runStage("SONAR-PREVIEW", vars) }
-    //dir("${vars.devopsRoot}/${vars.pipelinesPath}/stages/post-to-gerrit/") { commonLib.runStage("POST-TO-GERRIT", vars) }
-    //dir("${vars.devopsRoot}/${vars.pipelinesPath}/stages/sonar/") { commonLib.runStage("SONAR", vars) }
-    //dir("${vars.devopsRoot}/${vars.pipelinesPath}/stages/sonar-check/") { commonLib.runStage("SONAR-CHECK", vars) }
 }
+
