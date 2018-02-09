c80ab1d43875b380168cfb4cd0aa9408afe2e115
Viktor_Voronin <Viktor_Voronin@epam.com>
2018-02-09 15:50:01 +0300

    First release
    Change-Id: Ib04c8993eb9d6aed86841e0ba87d3a7df0d955b9

--- openshift/devops/pipelines/stages/find-rc.groovy
+++ openshift/devops/pipelines/stages/find-rc.groovy
@@ -5,14 +5,15 @@
 def run(vars) {
     //sh "git ls-remote --heads origin | grep RC | grep -o '\\..*[0-9]' | awk -F \".\" '{print \$3}' | sort -n -r | head -n 1"
     //sh "git ls-remote --heads origin | grep RC | grep -o '\\..*[0-9]' | awk -F \".\" '{print \$3}' | sort -n -r | head -n 1"
-    vars['RC'] = sh(
+    def RC1 = sh(
                 script: """
                         git ls-remote --heads origin | grep RC | grep -o '\\..*[0-9]' | awk -F "." '{print \$3}' | sort -n -r | head -n 1
                     """,
                 returnStdout: true
         ).trim()
-    RCnum=${vars.RC}.toInteger()+1
-    println("New RC number - ${RCnum}")
+    vars['RC']=RC1
+    vars['RCnum']=RC1.toInteger()+1
+    println("New RC number - ${vars.RCnum}")
 }
 
 return this;
