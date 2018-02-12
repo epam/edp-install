5203c6199036eb975b06037b2f01aab95290f57f
Viktor_Voronin <Viktor_Voronin@epam.com>
2018-02-12 14:24:15 +0300

    First release
    Change-Id: I75eba02fe8a9039f9394579964e231c1f72f44ae

---
+++ openshift/devops/pipelines/stages/check-branches.groovy
@@ -0,0 +1,29 @@
+/**
+ * Checkout from gerrit.  Parameters are defined by gerrit trigger
+ * @param vars object with all pipeline variables
+ */
+def run(vars) {
+    sh "rm -rf ${vars.workDir}*"
+    def failed=false
+    try {
+        dir("${vars.workDir}") {
+            checkout([$class                           : 'GitSCM', branches: [[name: "${vars.branch}"]],
+                      doGenerateSubmoduleConfigurations: false, extensions: [],
+                      submoduleCfg                     : [],
+                      userRemoteConfigs                : [[credentialsId: "${vars.credentials}",
+                                                           url    : "${vars.gitUrl}"]]])
+        }
+    } catch (Exception ex) {
+        failed=true
+    }
+    if (!failed)
+        commonLib.failJob("[JENKINS][ERROR] Release branch already exists")
+
+
+
+    this.result = "success"
+
+
+}
+
+return this;
--- openshift/devops/pipelines/stages/create-branch1.groovy
+++ openshift/devops/pipelines/stages/create-branch1.groovy
@@ -9,16 +9,10 @@ def run(vars) {
             sh """
                 eval `ssh-agent`
                 ssh-add ${key}
-                whoami
-                pwd
-                ls -al ~
                 ssh-keyscan -p ${GERRIT_PORT} ${GERRIT_HOST} >> ~/.ssh/known_hosts
-                git remote -v
-                git version
                 git checkout -b 0.1.${vars.RCnum}-${vars.prefix}
                 git push origin 0.1.${vars.RCnum}-${vars.prefix}"""
         }
-
 }
 
 return this;
