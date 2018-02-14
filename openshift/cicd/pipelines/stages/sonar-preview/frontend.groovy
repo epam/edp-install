0816b48d6f6a24b77b63fcf9757bd0d47f18908d
Alexander_Morozov <alexander_morozov@epam.com>
2018-02-14 16:48:13 +0300

    Fixed sonar preview stage for frontend
    Change-Id: I3c736a416bc67c19a18d4f1f653b8f53aa45e4d5

--- openshift/cicd/pipelines/stages/sonar-preview/frontend.groovy
+++ openshift/cicd/pipelines/stages/sonar-preview/frontend.groovy
@@ -6,7 +6,7 @@ def run(vars) {
     dir("${vars.serviceDir}") {
         def scannerHome = tool 'SonarQube Scanner'
         withSonarQubeEnv('Sonar') {
-            sh "${scannerHome}/bin/sonar-scanner -Dsonar.branch=${vars.serviceBranch}"
+            sh "${scannerHome}/bin/sonar-scanner -Dsonar.analysis.mode=preview -Dsonar.report.export.path=sonar-report.json -Dsonar.branch=precommit"
         }
     }
     this.result = "success"
