b34581a8003e58e4ba30c0d8acd3d120c32c701e
Yevgen Mospan <Yevgen_Mospan@epam.com>
2018-02-28 15:09:19 +0200

    test commit4
    Change-Id: Ie2c322ad14735db815ab074d83d80b564fcdd1eb

--- openshift/cicd/examples/3-tier-app/backend/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ openshift/cicd/examples/3-tier-app/backend/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -27,7 +27,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetClinicApplication {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Test commit3");
+        System.out.println("Test commit4");
         SpringApplication.run(PetClinicApplication.class, args);
     }
 
