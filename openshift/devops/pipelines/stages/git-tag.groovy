5a4827489753d8811d778ec9e8393fd92f473f5b
Alexander_Morozov <alexander_morozov@epam.com>
2018-03-14 17:45:41 +0300

    Adding git tag fixed
    Change-Id: Ib6dda9501adfd615d3b3f41e6b9190143ad66f3b

--- openshift/devops/pipelines/stages/git-tag.groovy
+++ openshift/devops/pipelines/stages/git-tag.groovy
@@ -1,9 +1,15 @@
 def run(vars) {
     dir("${vars.workDir}") {
-        sh "git config --global user.email \"${vars.gerritAutoUser}\""
-        sh "git config --global user.name \"${vars.gerritAutoUser}\""
-        sh "git tag -a ${vars.gitTag} -m 'Tag is added automatically by ${vars.gerritAutoUser} user'"
-        sh "git push --tags"
+        withCredentials([sshUserPrivateKey(credentialsId: "${vars.credentials}", keyFileVariable: 'key', passphraseVariable: '', usernameVariable: 'git_user')]) {
+            sh """
+                eval `ssh-agent`
+                ssh-add ${key}
+                ssh-keyscan -p ${vars.gerritSshPort} ${vars.gerritHost} >> ~/.ssh/known_hosts
+                git config --global user.email auto_epmc-java_vcs@epam.com
+                git config --global user.name auto_epmc-java_vcs
+                git tag -a ${vars.gitTag} -m 'Tag is added automatically by auto_epmc-java_vcs user'
+                git push --tags"""
+        }
     }
     this.result="success"
 }
