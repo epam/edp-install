a62a920166f6efe86432bee81c0e4087a79fbd02
Alexander_Morozov <alexander_morozov@epam.com>
2018-03-21 10:59:01 +0300

    [EPMDEDP-287] Reorganizing structure of the repository
    Change-Id: Ia39166b8ae74353f2239a116924583c3c8f22568

--- openshift/devops/pipelines/stages/push-to-nexus.groovy
+++
@@ -1,9 +0,0 @@
-
-def run(vars) {
-    dir("${vars.workDir}") {
-        sh "mvn deploy -DaltDeploymentRepository=nexus::default::${vars.artifact.repository} -DskipTests=true --settings ${vars.devopsRoot}/${vars.mavenSettings}"
-    }
-    this.result = "success"
-}
-
-return this;
