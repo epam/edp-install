3427315c66d7ef462686bd9f5aa84fde4e51e7e4
Alexander_Morozov <alexander_morozov@epam.com>
2018-04-17 20:08:55 +0300

    [EPMDEDP-413] Fixed get-project-to-delete stage
    Change-Id: I6c7bfc9a313c97d46bed53e240733922400d7506

--- application-pipelines/jenkins/stages/get-projects-to-delete.groovy
+++ application-pipelines/jenkins/stages/get-projects-to-delete.groovy
@@ -1,6 +1,9 @@
 def run(vars) {
     vars['projectsToDelete'] = sh(
-            script: "oc get project --output=custom-columns=NAME:.metadata.name --no-headers | egrep \'${vars.pipelineProject}-[0-9]+\' | grep -v ${vars.deployProject}",
+            script: "oc get project --no-headers " +
+                    "| egrep \'${vars.pipelineProject}-[0-9]+\' " +
+                    "| grep -v ${vars.deployProject} " +
+                    "| awk \'{print \$1}\'",
             returnStdout: true
     ).trim().tokenize('\n')
     println("[JENKINS][DEBUG] - Found suffixes - ${vars.projectsToDelete}")
