6e2e5daafa3dfbb71133c5faa296d15329d72c7b
Pavlo_Yemelianov <pavlo_yemelianov@epam.com>
2018-06-06 13:25:56 +0300

    [EPMDEDP-658] Cover 'copy autotest' functionality with autotests
    Change-Id: I5d83cf86e5119b8bb5828845e69811541bd55d60

--- auto-tests/src/test/java/com/epam/edp/sittests/smoke/AddAutotestSmokeTest.java
+++ auto-tests/src/test/java/com/epam/edp/sittests/smoke/AddAutotestSmokeTest.java
@@ -14,12 +14,30 @@ limitations under the License. */
 
 package com.epam.edp.sittests.smoke;
 
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.openshift.client.DefaultOpenShiftClient;
+import io.fabric8.openshift.client.OpenShiftClient;
+import io.qameta.allure.Feature;
+import io.restassured.http.ContentType;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpStatus;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import static com.epam.edp.sittests.smoke.StringConstants.*;
+import java.util.Map;
+
+import static com.epam.edp.sittests.smoke.StringConstants.GERRIT_PASSWORD;
+import static com.epam.edp.sittests.smoke.StringConstants.GERRIT_USER;
+import static com.epam.edp.sittests.smoke.StringConstants.JENKINS_PASSWORD;
+import static com.epam.edp.sittests.smoke.StringConstants.JENKINS_USER;
+import static com.epam.edp.sittests.smoke.StringConstants.OPENSHIFT_MASTER_URL;
+import static com.epam.edp.sittests.smoke.StringConstants.OPENSHIFT_PASSWORD;
+import static com.epam.edp.sittests.smoke.StringConstants.OPENSHIFT_TRUST_CERTS;
+import static com.epam.edp.sittests.smoke.StringConstants.OPENSHIFT_USERNAME;
+import static com.epam.edp.sittests.smoke.StringConstants.PRECOMMIT_PIPELINE_SUFFIX;
 import static io.restassured.RestAssured.given;
 
 /**
@@ -27,45 +45,97 @@ import static io.restassured.RestAssured.given;
  */
 public class AddAutotestSmokeTest {
     private static final String AUTOTESTS_NAME = "helloworld-autotests";
-    private static final String PRECOMMIT_AUTOTESTS_PIPELINE = PRECOMMIT_PIPELINE_SUFFIX + AUTOTESTS_NAME;
+    private static final String COPY_AUTOTEST_PREFIX = "copy-autotest";
+
+    private String copyAutotestName;
 
     private UrlBuilder urlBuilder;
+    private OpenShiftClient openShiftClient;
+
+    @Feature("Setup Openshift Client")
+    @BeforeClass
+    public void setUpOpenShiftClient() {
+        Config config = new ConfigBuilder().withMasterUrl(OPENSHIFT_MASTER_URL)
+                .withUsername(OPENSHIFT_USERNAME)
+                .withPassword(OPENSHIFT_PASSWORD)
+                .withTrustCerts(OPENSHIFT_TRUST_CERTS)
+                .build();
+
+        this.openShiftClient = new DefaultOpenShiftClient(config);
+    }
 
     @BeforeClass
     @Parameters("ocpEdpSuffix")
     public void setUp(String ocpEdpSuffix) {
+        this.copyAutotestName = COPY_AUTOTEST_PREFIX + "-" + ocpEdpSuffix;
         this.urlBuilder = new UrlBuilder(ocpEdpSuffix);
     }
 
-    @Test
-    public void testGerritProjectWasCreated() {
-        given()
-            .pathParam("project", AUTOTESTS_NAME)
-            .auth()
-            .basic(GERRIT_USER, GERRIT_PASSWORD)
-        .when()
-            .get(urlBuilder.buildUrl("http",
-                    "gerrit",
-                    "edp-cicd",
-                    "a/projects/{project}"))
-        .then()
-            .statusCode(HttpStatus.SC_OK);
+    @DataProvider(name = "autotest")
+    public Object[][] autotest() {
+        return new Object[][]{{AUTOTESTS_NAME}, {copyAutotestName}};
+    }
+
+    @Test(dataProvider = "autotest")
+    public void testGerritProjectWasCreated(String autotest) {
+        given().log().all()
+                .pathParam("project", autotest)
+                .auth()
+                .basic(GERRIT_USER, GERRIT_PASSWORD)
+                .when()
+                .get(urlBuilder.buildUrl("http",
+                        "gerrit",
+                        "edp-cicd",
+                        "a/projects/{project}"))
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test(dataProvider = "autotest")
+    public void testJenkinsPipelineWasCreated(String autotest) {
+        String pipeline = PRECOMMIT_PIPELINE_SUFFIX + autotest;
+        given().log().all()
+                .pathParam("pipeline", pipeline)
+                .auth()
+                .preemptive()
+                .basic(JENKINS_USER, JENKINS_PASSWORD)
+                .when()
+                .get(urlBuilder.buildUrl("http",
+                        "jenkins",
+                        "edp-cicd",
+                        "job/{pipeline}/api/json"))
+                .then()
+                .statusCode(HttpStatus.SC_OK);
     }
 
     @Test
-    public void testJenkinsPipelineWasCreated() {
-        given()
-            .pathParam("pipeline", PRECOMMIT_AUTOTESTS_PIPELINE)
-            .auth()
-            .preemptive()
-            .basic(JENKINS_USER, JENKINS_PASSWORD)
-        .when()
-            .get(urlBuilder.buildUrl("http",
-                    "jenkins",
-                    "edp-cicd",
-                    "job/{pipeline}/api/json"))
-        .then()
-            .statusCode(HttpStatus.SC_OK);
-    }
+    public void testCopyAutotestProjectWasCreatedInGitGroupRepo() {
+        Map<String, String> credentials = openShiftClient.secrets()
+                .inNamespace("edp-deploy")
+                .withName("auto-epmc-java-vcs-with-ssh")
+                .get()
+                .getData();
+        String username = new String(Base64.decodeBase64(credentials.get("username"))).trim();
+        String password = new String(Base64.decodeBase64(credentials.get("password"))).trim();
 
+        String token = given()
+                .contentType(ContentType.URLENC)
+                .param("grant_type", "password")
+                .param("username", username)
+                .param("password", password)
+                .post("https://git.epam.com/oauth/token")
+                .then()
+                .extract()
+                .path("access_token");
+
+        given().log().all()
+                .auth()
+                .preemptive()
+                .oauth2(token)
+                .urlEncodingEnabled(false)
+                .when()
+                .get("https://git.epam.com/api/v4/projects/epmd-edp%2Ftemp%2F" + copyAutotestName)
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+    }
 }
--- auto-tests/src/test/java/com/epam/edp/sittests/smoke/AddEnvironmentSmokeTest.java
+++ auto-tests/src/test/java/com/epam/edp/sittests/smoke/AddEnvironmentSmokeTest.java
@@ -14,71 +14,53 @@ limitations under the License. */
 
 package com.epam.edp.sittests.smoke;
 
-import io.fabric8.openshift.api.model.Template;
-import io.fabric8.openshift.client.OpenShiftClient;
 import io.qameta.allure.Feature;
 import org.apache.http.HttpStatus;
-import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import io.fabric8.kubernetes.client.Config;
-import io.fabric8.kubernetes.client.ConfigBuilder;
-import io.fabric8.openshift.client.DefaultOpenShiftClient;
 
-import static com.epam.edp.sittests.smoke.StringConstants.*;
+import static com.epam.edp.sittests.smoke.StringConstants.DELETION_PIPELINE_NAME;
+import static com.epam.edp.sittests.smoke.StringConstants.JENKINS_PASSWORD;
+import static com.epam.edp.sittests.smoke.StringConstants.JENKINS_USER;
+import static com.epam.edp.sittests.smoke.StringConstants.OPENSHIFT_CICD_NAMESPACE;
 import static io.restassured.RestAssured.given;
 
 public class AddEnvironmentSmokeTest {
-    private static String sitPipelineName;
-    private static String qaPipelineName;
-    private static String openshiftNamespace;
-    private static String ocpEdpSuffix;
+    private String sitPipelineName;
+    private String qaPipelineName;
+    private String openshiftNamespace;
     private UrlBuilder urlBuilder;
-    private OpenShiftClient openShiftClient;
 
     @Feature("Setup URL Builder")
     @BeforeClass
     @Parameters("ocpEdpSuffix")
     public void setUp(String ocpEdpSuffix) {
         this.urlBuilder = new UrlBuilder(ocpEdpSuffix);
-        this.ocpEdpSuffix = ocpEdpSuffix;
         this.sitPipelineName = "sit-" + ocpEdpSuffix + "-deploy-pipeline";
         this.qaPipelineName = "qa-" + ocpEdpSuffix + "-deploy-pipeline";
         this.openshiftNamespace = OPENSHIFT_CICD_NAMESPACE + "-" + ocpEdpSuffix;
     }
 
     @DataProvider(name = "pipeline")
-    public static Object[][] pipeline() {
-        return new Object[][] { {sitPipelineName}, {qaPipelineName}, {DELETION_PIPELINE_NAME}};
-    }
-
-    @Feature("Setup Openshift Client")
-    @BeforeClass
-    public void setUpOpenShiftClient() {
-        Config config = new ConfigBuilder().withMasterUrl(OPENSHIFT_MASTER_URL)
-                .withUsername(OPENSHIFT_USERNAME)
-                .withPassword(OPENSHIFT_PASSWORD)
-                .withTrustCerts(OPENSHIFT_TRUST_CERTS)
-                .build();
-
-        this.openShiftClient = new DefaultOpenShiftClient(config);
+    public Object[][] pipeline() {
+        return new Object[][]{{sitPipelineName}, {qaPipelineName}, {DELETION_PIPELINE_NAME}};
     }
 
     @Test(dataProvider = "pipeline")
     public void testJenkinsTestPipelinesHasBeenCreated(String pipeline) {
         given().log().all()
-            .pathParam("pipeline", pipeline)
-            .pathParam("folder", openshiftNamespace)
-            .auth()
-            .preemptive()
-            .basic(JENKINS_USER, JENKINS_PASSWORD)
-        .when()
-            .get(urlBuilder.buildUrl("https",
-                    "jenkins",OPENSHIFT_CICD_NAMESPACE,
-                    "job/{folder}/job/{folder}-{pipeline}/api/json"))
-        .then()
-            .statusCode(HttpStatus.SC_OK);
+                .pathParam("pipeline", pipeline)
+                .pathParam("folder", openshiftNamespace)
+                .auth()
+                .preemptive()
+                .basic(JENKINS_USER, JENKINS_PASSWORD)
+                .when()
+                .get(urlBuilder.buildUrl("https",
+                        "jenkins", OPENSHIFT_CICD_NAMESPACE,
+                        "job/{folder}/job/{folder}-{pipeline}/api/json"))
+                .then()
+                .statusCode(HttpStatus.SC_OK);
     }
 }
