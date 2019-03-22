dd2bf2d9f7dae5e6181d5d7bebe1a6985988c5aa
Viktor_Voronin <viktor_voronin@epam.com>
2019-03-22 14:26:59 +0300

    [EPMDEDP-2178] Add create Create-release pipeline in Jobs_provisioning
    Change-Id: I2ba6238f9907879c900784d593103209a7ec6c7a

--- ansible-playbooks/roles/post-integration/templates/jenkins/jobsprovisioning.groovy
+++ ansible-playbooks/roles/post-integration/templates/jenkins/jobsprovisioning.groovy
@@ -1,4 +1,4 @@
-/* Copyright 2018 EPAM Systems.
+/* Copyright 2019 EPAM Systems.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ limitations under the License. */
 
 import groovy.json.*
 
-def createPipeline(pipelineName, applicationName, applicationStages, pipelineScript, pipelinePath, devopsRepository) {
+def createCiPipeline(pipelineName, applicationName, applicationStages, pipelineScript, devopsRepository) {
     pipelineJob("${pipelineName}") {
         logRotator {
             numToKeep(10)
@@ -41,10 +41,37 @@ def createPipeline(pipelineName, applicationName, applicationStages, pipelineScr
                     }
                 }
                 parameters {
-                    stringParam("STAGES", "${applicationStages}","")
-                    stringParam("GERRIT_PROJECT_NAME", "${applicationName}","")
+                    stringParam("STAGES", "${applicationStages}", "")
+                    stringParam("GERRIT_PROJECT_NAME", "${applicationName}", "")
                     if (pipelineName.contains("Build"))
-                        stringParam("BRANCH", "master","")
+                        stringParam("BRANCH", "master", "")
+                }
+            }
+        }
+    }
+}
+
+def createReleasePipeline(pipelineName, applicationName, applicationStages, pipelineScript, devopsRepository) {
+    pipelineJob("${pipelineName}") {
+        logRotator {
+            numToKeep(14)
+            daysToKeep(30)
+        }
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote { url(devopsRepository) }
+                        branches("master")
+                        scriptPath("${pipelineScript}")
+                    }
+                }
+                parameters {
+                    stringParam("STAGES", "${applicationStages}", "")
+                    if (pipelineName.contains("Create-release")) {
+                        stringParam("GERRIT_PROJECT", "${applicationName}", "")
+                        stringParam("RELEASE_NAME", "", "")
+                    }
                 }
             }
         }
@@ -65,20 +92,21 @@ stages['Build-gradle'] = stages['Build-maven']
 stages['Build-dotnet'] = "[{\"name\": \"checkout\"},{\"name\": \"get-version\"},{\"name\": \"compile\"}," +
         "{\"name\": \"tests\"},{\"name\": \"sonar\"},{\"name\": \"build-image\"}," +
         "{\"name\": \"push\"},{\"name\": \"git-tag\"}]"
+stages['Create-release'] = "[{\"name\": \"checkout\"},{\"name\": \"create-branch\"}]"
 
 ['app.settings.json', 'auto-test.settings.json'].each() { settingsFile ->
     new JsonSlurperClassic().parseText(new File("${JENKINS_HOME}/project-settings/${settingsFile}").text).each() { item ->
         def applicationName = item.name
         if (settingsFile == 'app.settings.json') {
-
-            createPipeline("Code-review-${applicationName}", applicationName, stages['Code-review-application'],
-                    "code-review.groovy", "", "${appRepositoryBase}/${item.name}")
-            createPipeline("Build-${applicationName}", applicationName, stages["Build-${item.build_tool.toLowerCase()}"],
-                    "build.groovy", "", "${appRepositoryBase}/${item.name}")
-        }
-        else
-            createPipeline("Code-review-${applicationName}", applicationName, stages['Code-review-autotest'],
-                    "code-review.groovy", "", "${appRepositoryBase}/${item.name}")
+            createCiPipeline("Code-review-${applicationName}", applicationName, stages['Code-review-application'],
+                    "code-review.groovy", "${appRepositoryBase}/${item.name}")
+            createCiPipeline("Build-${applicationName}", applicationName, stages["Build-${item.build_tool.toLowerCase()}"],
+                    "build.groovy", "${appRepositoryBase}/${item.name}")
+            createReleasePipeline("Create-release-${applicationName}", applicationName, stages["Create-release"],
+                    "create-release.groovy", "${appRepositoryBase}/${item.name}")
+        } else
+            createCiPipeline("Code-review-${applicationName}", applicationName, stages['Code-review-autotest'],
+                    "code-review.groovy", "${appRepositoryBase}/${item.name}")
 
     }
 }
@@ -88,9 +116,10 @@ if (Boolean.valueOf("${PARAM}")) {
     def type = "${TYPE}"
     def buildTool = "${BUILD_TOOL}"
     if (type == "app") {
-        createPipeline("Code-review-${appName}", appName, stages['Code-review-application'], "code-review.groovy", "", "${appRepositoryBase}/${appName}")
-        createPipeline("Build-${appName}", appName, stages["Build-${buildTool}"], "build.groovy", "", "${appRepositoryBase}/${appName}")
+        createCiPipeline("Code-review-${appName}", appName, stages['Code-review-application'], "code-review.groovy", "${appRepositoryBase}/${appName}")
+        createCiPipeline("Build-${appName}", appName, stages["Build-${buildTool}"], "build.groovy", "${appRepositoryBase}/${appName}")
+        createReleasePipeline("Create-release-${appName}", appName, stages["Create-release"], "create-release.groovy", "${appRepositoryBase}/${appName}")
     } else {
-        createPipeline("Code-review-${appName}", appName, stages['Code-review-autotest'], "code-review.groovy", "", "${appRepositoryBase}/${appName}")
+        createCiPipeline("Code-review-${appName}", appName, stages['Code-review-autotest'], "code-review.groovy", "${appRepositoryBase}/${appName}")
     }
 }
